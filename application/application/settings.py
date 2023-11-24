0b15429b183e7709021e354b0d27bc4a4ab2be6a
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-11-24 17:34:49 +0200

    Change settings

--- application/application/settings.py
+++ application/application/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'NAME': env.str('POSTGRES_DB'),
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'host.docker.internal',
         'PORT': '5432',
     }
 }
